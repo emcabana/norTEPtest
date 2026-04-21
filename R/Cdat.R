07b63e3b48fef548d89720f0287eb8dee95159ff
emcabana <66531410+emcabana@users.noreply.github.com>
2026-04-21 16:30:26 -0300

    Add files via upload

---
+++ R/Cdat.R
@@ -0,0 +1,6 @@
+#' @title Precomputed C matrix
+#' @description Example covariance matrix used by TEPest
+#' @format A matrix with dimensions (l+1) x (l+1)
+#' @usage data(Cdat)
+#' @keywords internal
+"Cdat"
